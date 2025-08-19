bf50145583862f4ff458d6691fb23d5b05ad0011
Handre1996 <barend@dreamlandtechnology.co.za>
2025-08-19 15:57:24 +0200

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -177,5 +177,6 @@ export default function Hero() {
       {/* Bottom gradient fade */}
       <div className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-t from-background to-transparent"></div>
     </div>
+    </div>
   );
 }
