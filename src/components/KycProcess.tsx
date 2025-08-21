a03e09b463c22790a3a73a88aeffd2d45124b701
Handre1996 <barend@dreamlandtechnology.co.za>
2025-08-21 12:33:48 +0200

    Updated KycProcess.tsx

--- src/components/KycProcess.tsx
+++ src/components/KycProcess.tsx
@@ -130,7 +130,7 @@ export default function KycProcess() {
             {/* Content Section */}
             <div className="p-8 lg:p-12 flex flex-col justify-center order-1 lg:order-2">
               <h3 className="text-2xl font-heading font-bold text-blanc mb-8">
-                Why Choose Us
+                Required Documents
               </h3>
               <div className="space-y-4">
                 {requiredDocuments.map((document, index) => (
