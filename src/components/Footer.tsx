f7d4888b06ea5c862d346ac5ad80c985807e710b
Handre1996 <barend@dreamlandtechnology.co.za>
2025-08-22 16:12:48 +0200

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -33,7 +33,7 @@ export default function Footer() {
                   <img 
                   src="/LiquidCurrent_Logo_CMYK_PrimaryLogoTeal.png" 
                   alt="Liquid Current" 
-                  className="h-42 w-auto opacity-90 hover:opacity-100 transition-opacity duration-200"
+                  className="h-[42px] w-auto opacity-90 hover:opacity-100 transition-opacity duration-200"
                 />
             </div>
           </div>
