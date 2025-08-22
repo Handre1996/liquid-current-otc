b4d1c0d45b43c1eb33db45bdaeb996972c326df7
Handre1996 <barend@dreamlandtechnology.co.za>
2025-08-22 14:41:37 +0200

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -34,7 +34,7 @@ export default function Hero() {
           className="w-full h-full object-cover"
           style={{
             objectPosition: "80% 20%",     // shift the focal point left (default is 50% 50%)
-            transform: "scale(1.4)",      // zoom in
+            transform: "scale(1.6)",      // zoom in
             transformOrigin: "left center" // anchor zooming from the left instead of center
           }}
         />
