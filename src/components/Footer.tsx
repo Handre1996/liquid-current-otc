5e672a21c7c3d0e4a802eab5c6337d4cac080926
Handre1996 <barend@dreamlandtechnology.co.za>
2025-08-22 15:51:59 +0200

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -35,7 +35,7 @@ export default function Footer() {
                 <img 
                   src="/LiquidCurrent_Logo_CMYK_PrimaryLogoFoam.png" 
                   alt="Liquid Current" 
-                  className="h-36 w-auto opacity-80 hover:opacity-100 transition-opacity duration-200"
+                  className="h-37 w-auto opacity-80 hover:opacity-100 transition-opacity duration-200"
                 />
               ) : (
                 <img 
