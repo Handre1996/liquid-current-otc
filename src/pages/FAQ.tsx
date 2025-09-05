560d7cb2a62ab031e3b55b5f9062bb8469865064
Handre1996 <barend@dreamlandtechnology.co.za>
2025-09-05 11:12:15 +0200

    Updated FAQ.tsx

--- src/pages/FAQ.tsx
+++ src/pages/FAQ.tsx
@@ -302,7 +302,7 @@ const FAQ: React.FC = () => {
 <div
   className="
     relative lg:col-span-1 overflow-hidden
-    h-28 sm:h-32 md:h-40        /* <<< make mobile shorter here */
+    h-32 sm:h-36 md:h-44        /* <<< make mobile shorter here */
     lg:h-auto lg:min-h-[500px]  /* desktop: original tall block */
   "
 >
